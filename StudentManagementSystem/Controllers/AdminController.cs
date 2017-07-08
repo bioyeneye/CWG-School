dbe846b59865a173b660da16f7bb423a957c13dc
Oyeneye Bolaji <bioyeneye@gmail.com>
2017-07-07 23:17:23 -0700

    Initial commit

---
+++ StudentManagementSystem/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentManagementSystem.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Students()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult Teachers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult Courses()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
